17d3ddea0068579a02214af98af1b0abcbdde2ab
FillArt <artiom.filipopovschii@gmail.com>
2025-01-17 17:11:30 -0400

    1. describe the MessageType

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,22 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type User = {
+    avatar: string;
+    name: string;
+}
+
+export type Message = {
+    text: string;
+    time: string;
+
+}
+
+export type MessageType = {
+    id: number;
+    user: User;
+    message: Message;
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {

928b5500d3bea66a814ca72323a0f57179f7462b
FillArt <artiom.filipopovschii@gmail.com>
2025-01-17 22:29:47 -0400

    4 - add typing of props in the GreetingContainer.tsx file

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -34,7 +34,7 @@ export const pureAddUserCallback = (name: string, setUsers: (user: UserType[]) =
 const HW3 = () => {
     const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 

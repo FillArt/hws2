9717eb9718163876617d66e92ef9153c507e70c5
FillArt <artiom.filipopovschii@gmail.com>
2025-01-17 20:05:04 -0400

    6 - add the type and logic of the deleteAffairCallback function

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -47,8 +47,8 @@ function HW2() {
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
-        // need to fix
+    const deleteAffairCallback = (_id: number) => { // need to fix any
+        setAffairs(deleteAffair(affairs, _id))
     }
 
     return (

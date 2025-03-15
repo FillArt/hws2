bec0d68a0eb0555c1f513dd6e01f3ab1073b0785
FillArt <artiom.filipopovschii@gmail.com>
2025-03-15 09:55:43 -0300

    3 - add the setLoading function

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -9,18 +9,23 @@ import {Loader} from './Loader'
 /*
 *** 1 - в файле loadingReducer.ts дописать типы и логику
 *** 2 - получить isLoading из редакса
-* 3 - дописать функцию setLoading
+*** 3 - дописать функцию setLoading
 * 4 - сделать стили в соответствии с дизайном
 * */
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
+    const dispatch = useDispatch()
     const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading)
     console.log(isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
+        dispatch(loadingAC(true))
 
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
         // setTimeout
     }
 

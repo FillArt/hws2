abf8ce291f691eb31129791d52fe66edbc9a9584
FillArt <artiom.filipopovschii@gmail.com>
2025-03-15 10:15:09 -0300

    4 - make styles according to design

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,9 @@
 import s from './Loader.module.css'
+import ellipse from './ellipse.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+    <div className={s.loader}>
+        <img src={ellipse} alt=""/>
+    </div>
+)
+

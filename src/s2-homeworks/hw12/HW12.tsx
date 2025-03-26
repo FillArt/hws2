8113cdb959406b347bc1de53c98af0e6df6c1291
FillArt <artiom.filipopovschii@gmail.com>
2025-03-26 16:29:00 -0300

    2 - get themeId from redux

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -4,10 +4,11 @@ import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
 import {changeThemeId} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 *** 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
-* 2 - получить themeId из редакса
+*** 2 - получить themeId из редакса
 * 3 - дописать тип и логику функции change
 * 4 - передать пропсы в SuperSelect
 * */
@@ -20,6 +21,8 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
+    const theme = useSelector((state: AppStoreType) => state.theme.themeId)
+    console.log(theme)
     const themeId = 1
 
     const change = (id: any) => { // дописать функцию
